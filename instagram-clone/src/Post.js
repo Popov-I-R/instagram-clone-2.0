434230fbca29a2a0cb6c5b10f7f715cf886b8276
Popov-I-R <farm03007@gmail.com>
2022-03-09 21:55:58 +0200

    Comment section + post button added.

--- instagram-clone/src/Post.js
+++ instagram-clone/src/Post.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./Post.css";
 import Avatar from "@mui/material/Avatar";
-// import Avatar from "material-ui/core/Avatar";
+import { db } from "./firebase";
 
-function Post({ username, caption, imageUrl }) {
-  
+function Post({ postId, username, caption, imageUrl }) {
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState('')
+
+  let unsubscribe;
+  if(postId) {
+    unsubscribe = db
+    .collection("posts")
+    .doc(postId)
+    .collection("comments")
+    .onSnapshot((snapshot) => {
+      setComments(snapshot.docs.map((doc) => doc.data()))
+    })
+  }
+//   return() => {
+//     unsubscribe()
+//   };
+// }, [postId]
+
+const postComment = (event) => {
+
+}
 
   return (
     <div className="post">
       <div className="post__header">
         <Avatar
           className="post__avatar"
-          alt="Remy Sharp"
+          alt="Avatar"
           src="https://thumbs.dreamstime.com/b/user-profile-icon-creative-trendy-colorful-round-button-illustration-isolated-156511788.jpg"
         />
 
@@ -27,6 +47,25 @@ function Post({ username, caption, imageUrl }) {
         <strong>{username}</strong> {caption}
       </h4>
       {/* username + caption */}
+
+    <form className="post__commentBox">
+      <input 
+        className="post__input"
+        type="text"
+        placeholder="Add a comment..."
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      />
+      <button
+        disabled={!comment}
+        className="post__button"
+        type="submit"
+        onClick={postComment}
+        >
+        Post
+      </button>
+    </form>
+
     </div>
   );
 }
